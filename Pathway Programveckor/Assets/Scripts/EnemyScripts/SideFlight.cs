871e6c64af7fa2d31b637b46bed2385fddd53889
marreHovmollerLBS <marius.hovmoller@elev.ga.lbs.se>
2025-01-12 19:37:09 +0100

    SideFlight Script
    Ett nytt script, typ samma som myrans men den här rör sig i luften

--- Pathway Programveckor/Assets/Scripts/EnemyScripts/SideFlight.cs
+++ Pathway Programveckor/Assets/Scripts/EnemyScripts/SideFlight.cs
@@ -1,48 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 
-public class GoombaWalk : MonoBehaviour
+public class SideFlight : MonoBehaviour
 {
     Rigidbody2D rb;
     public float speed = 2f;
     public float checkRadius = 0.1f;
-    public LayerMask groundLayer;
+    public LayerMask antiborderLayer;
     public Transform checkRight;
     public Transform checkLeft;
-    
+
     private bool movingRight = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        Debug.Log("GoombaWalk.cs start");
+        Debug.Log("SideFlight.cs start");
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-        if (!IsGroundAtPoint(checkRight))
+        SideMovement();
+    }
+
+    void SideMovement()
+    {
+        if (!IsAntiborderAtPoint(checkRight))
         {
-            Debug.Log("Det finns ingen mark till höger");
-            movingRight=false;
+            Debug.Log("Det finns ingen antiborder till höger");
+            movingRight = false;
         }
 
-        if (!IsGroundAtPoint(checkLeft))
+        if (!IsAntiborderAtPoint(checkLeft))
         {
-            Debug.Log("Det finns ingen mark till vänster");
+            Debug.Log("Det finns ingen antiborder till vänster");
             movingRight = true;
         }
 
         int direction = movingRight ? 1 : -1;
         rb.velocity = new Vector2(speed * direction, rb.velocity.y);
     }
-
-    bool IsGroundAtPoint(Transform checkPoint)
+    bool IsAntiborderAtPoint(Transform checkPoint)
     {
-        return Physics2D.OverlapCircle(checkPoint.position, checkRadius, groundLayer);
+        return Physics2D.OverlapCircle(checkPoint.position, checkRadius, antiborderLayer);
     }
+
 }
