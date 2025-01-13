abdf20bf3cc916c95df9600de0b6e0091c59a2be
Leo4075 <leo.mollberg@gmail.com>
2025-01-13 16:32:04 +0100

    Gun firerate Cooldown

--- Pathway Programveckor/Assets/Scripts/Shooting.cs
+++ Pathway Programveckor/Assets/Scripts/Shooting.cs
@@ -6,21 +6,40 @@ public class GunController : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform bulletSpawnPos;
 
+    public float cooldownTimer = 1f;
+    private float cooldown;
+    private bool isCooldownOn = false;
+
     private void Start()
     {
-
+        
     }
 
     void Update()
     {
+        Debug.Log(cooldown);
+
         if (Input.GetKey(KeyCode.Mouse0))
         {
-            shoot();
+            if(isCooldownOn == false)
+            {
+                shoot();
+            }
+        }
+        if(isCooldownOn == true)
+        {
+            cooldown -= Time.deltaTime;
+            if(cooldown <= 0)
+            {
+                isCooldownOn = false;
+                cooldown = cooldownTimer;
+            }
         }
     }
 
     void shoot()
     {
+        isCooldownOn = true;
         Instantiate(bulletPrefab, bulletSpawnPos);
     }
 }
