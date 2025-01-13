896152bb134ec64a09fbee5f425ec3b162af6559
Leo4075 <leo.mollberg@gmail.com>
2025-01-13 15:58:54 +0100

    Bullet camera
    bullets deletes themselves after exiting the camera view

--- Pathway Programveckor/Assets/Scripts/BulletBehaviour.cs
+++ Pathway Programveckor/Assets/Scripts/BulletBehaviour.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 public class BulletBehaviour : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float bulletSpeed = 20f; 
+    public float bulletSpeed = 20f;
+
+    public Camera mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
         rb.velocity = new Vector2(bulletSpeed, 0);
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        rb.gravityScale = 10;
+        Vector3 viewportPos = mainCamera.WorldToViewportPoint(transform.position);
+
+        // Check if the object is outside the screen
+        if (viewportPos.x < 0f || viewportPos.x > 1f || viewportPos.y < 0f || viewportPos.y > 1f)
+        {
+            Destroy(gameObject);
+            // You can perform actions when the object is off-screen, like disabling it or triggering an event
+        }
     }
 }
