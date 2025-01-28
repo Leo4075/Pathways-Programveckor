6765b79bbf90a4bdf75d4c175d32667a01af0a4e
Leo4075 <leo.mollberg@gmail.com>
2025-01-28 14:20:46 +0100

    Xp changes

---
+++ Pathway Programveckor/Assets/Scripts/Player/XPmanager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XPmanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReceiveMessage()
+    {
+        Debug.Log("Myra Died");
+    }
+}
