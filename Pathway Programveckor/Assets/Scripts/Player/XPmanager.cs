0bfd78fdda52630985e66b7d4ca97c965b4ec61e
Leo4075 <leo.mollberg@gmail.com>
2025-01-31 10:31:09 +0100

    Hej

--- Pathway Programveckor/Assets/Scripts/Player/XPmanager.cs
+++ Pathway Programveckor/Assets/Scripts/Player/XPmanager.cs
@@ -7,11 +7,14 @@ public class XPmanager : MonoBehaviour
     private int xp;
     private int xpCurrentLevel = 1;
     public int xpLevelUp = 20;
+    public int coefficient = 10;
+    public int initialXPCost = 20;
 
     // Start is called before the first frame update
     void Start()
     {
         xp = 0;
+        xpLevelUp = 20;
     }
 
     // Update is called once per frame
@@ -21,10 +24,16 @@ public class XPmanager : MonoBehaviour
         {
             xp -= xpLevelUp;
             xpCurrentLevel += 1;
-            Debug.Log("total xp = " + xp + " Current level " + xpCurrentLevel);
+            Debug.Log("total xp = " + xp + " Current level " + xpCurrentLevel + " xpLevelUp " + xpLevelUp);
+            xpLevelUp = RequiredXPIncrease();
+           
         }
     }
 
+    public int RequiredXPIncrease()
+    {
+        return coefficient * xpCurrentLevel + initialXPCost;
+    }
     public void MyrDeath()
     {
         xp += 10;
